bcacca80b252c04804d098e023625994719d8fc7
Thiago Vasconcelos <pedrosav14@gmail.com>
2022-10-10 19:48:14 -0300

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { Task } from 'src/models/task.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'todo-list';
+  public title: String = 'Todo List';
+  public tasks: Task[] = [];
+
+  constructor() {
+    this.tasks.push(new Task(1,"developer todolist angular version", "programming", false))
+    this.tasks.push(new Task(2,"finish the 2nd LOTR book", "personal", false))
+    this.tasks.push(new Task(3,"go to the gym", "personal", true))
+    
+  }
 }
---
+++ src/models/task.model.ts
@@ -0,0 +1,12 @@
+export class Task {
+
+    constructor(
+        public id: Number,
+        public title: String,
+        public category: String,
+        public done: Boolean
+
+    ) {
+     
+    }
+}
