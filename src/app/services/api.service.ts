b4c2b7469f0cc1f679b51c7728ee44a6fcfb8172
Thiago Vasconcelos <pedrosav14@gmail.com>
2022-11-29 19:12:14 -0300

    api consuming updated

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,18 +1,23 @@
 import { HttpClient, HttpRequest, JsonpInterceptor } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Todo } from 'src/models/todo.model';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-   api_server_url: string = "http://127.0.0.1:8000";
+  
+  api_server_url: string = "http://127.0.0.1:8000";
   
   constructor(private http: HttpClient) { 
 
   }
 
-  public getTodos(){
-    return this.http.get(`${this.api_server_url}`)
+  public getTodos(): Observable<Todo>{
+     return this.http.get<Todo>(this.api_server_url)
+
   }
 
 }
