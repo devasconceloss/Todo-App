1265d40ccfee85d57245b6542c289374bc228f26
Thiago Vasconcelos <pedrosav14@gmail.com>
2022-12-02 14:55:18 -0300

    api service moved todolist,sharing data with input

--- src/app/components/todo-content/todo-content.component.ts
+++ src/app/components/todo-content/todo-content.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { faCheck } from '@fortawesome/free-solid-svg-icons';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
 import { ApiService } from 'src/app/services/api.service';
@@ -11,18 +11,22 @@ import { Todo } from 'src/models/todo.model';
   styleUrls: ['./todo-content.component.css']
 })
 export class TodoContentComponent implements OnInit {
+  todos: Todo[] = [];
+  @Input() todo: Todo;
+  @Output() deleteTodo: EventEmitter<Todo> = new EventEmitter
   faCheck = faCheck;
   faTimes = faTimes;
-  todos: Todo[] = [];
 
-  constructor(private apiService: ApiService) { }
+  constructor() { }
 
   ngOnInit() {
-    this.apiService.getTodos().subscribe((todoData) => {
-      this.todos = todoData['todos']
-      console.log(this.todos)
+
     
-  });
+  }
+
+  deletingTodo(todo: Todo){
+    this.deleteTodo.emit(todo)
+  }
 }
     
-}
+
