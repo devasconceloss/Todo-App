27c720cea3a8ba8e00be8559a9948bd1ea42cf30
Thiago Vasconcelos <pedrosav14@gmail.com>
2022-12-06 20:57:23 -0300

    addnew todo function and event emitter implemented

--- src/app/components/addtask/addtask.component.ts
+++ src/app/components/addtask/addtask.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Todo } from 'src/models/todo.model';
+
 
 @Component({
   selector: 'app-addtask',
@@ -11,6 +12,14 @@ export class AddtaskComponent implements OnInit {
   public title: String = 'Todo List';
   public todos: Todo[] = [];
   public form: FormGroup;
+  public new_todo: Todo;
+
+  public id: number = 0;
+  public title_todo: string;
+  public category: string;
+  public done: boolean
+  @Output() newTodo: EventEmitter<Todo> = new EventEmitter
+
 
   constructor(private fb:FormBuilder) { 
 
@@ -29,8 +38,15 @@ export class AddtaskComponent implements OnInit {
 
   }
   
-  addTask(){
-    console.log("ok")
+  addNewTodo(){
+    let title_todo = this.form.controls['title'].value
+    let category= this.form.controls['category'].value
+    let done = false
+
+    this.new_todo = new Todo(this.id + 1, title_todo, category, done)
+    console.log(this.new_todo)
+
+    this.newTodo.emit(this.new_todo)
   }
 
   ngOnInit() {
