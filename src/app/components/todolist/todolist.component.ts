62ac4abd0c3cdbf9b30252867c032029b42fab30
Thiago Vasconcelos <pedrosav14@gmail.com>
2022-12-06 20:55:44 -0300

    add task to api working, but id isn't increasing

--- src/app/components/todolist/todolist.component.ts
+++ src/app/components/todolist/todolist.component.ts
@@ -12,87 +12,13 @@ import { ApiService } from '../../services/api.service';
 export class TodolistComponent implements OnInit {
   public title: String = 'Todo List';
   todos: Todo[] = [];
+  todo: Todo;
 
   constructor(private apiService: ApiService) { 
 
 
   }
 
-/*
-  addTask(){
-    const id = this.todos.length + 1
-    const title = this.form.controls['title'].value
-    let category = this.form.controls['category'].value
-    const done = false
-
-    switch(category){
-      case "Work":
-        category = "👜"
-        break;
-      case "Health":
-        category = "💓"
-        break;
-      case "Fun":
-        category = "😛"
-        break;
-      case "Other":
-        category = "➕"
-        break;
-    }
-
-
-
-    this.savingOnLocal("todos")
-    this.erasePreviousData()
-    
-  }
-
-  removeTask(todo: Todo): void {
-    const taskIndex = this.todos.indexOf(todo)
-    if(taskIndex != -1){
-      this.todos.splice(taskIndex, 1)
-      const localData = JSON.parse(localStorage.getItem("todos"))
-      localData.splice(taskIndex,1)
-      
-      
-      this.attData(localData)
-
-    } 
-  
-  }
-
-  attData(data: any){
-    const attData = JSON.stringify(data)
-    localStorage.setItem("todos", attData)
-  }
-
-  finishTask(todo: Todo){
-    todo.done = true
-
-  }
-
-  reopenTask(todo: Todo){
-    todo.done = false
-  }
-
-  erasePreviousData(){
-    this.form.reset()
-  }
-
-  savingOnLocal(data: string){
-    const localData = JSON.stringify(this.todos)
-    localStorage.setItem(data, localData)
-  }
-
-  loadPreviousTasks(){
-    const data = localStorage.getItem("todos")
-    if(data){
-      this.todos = JSON.parse(data)
-    } else {
-      this.todos = []
-    }
-   */ 
-  
 
   ngOnInit() {
     this.apiService
@@ -100,6 +26,7 @@ export class TodolistComponent implements OnInit {
     .subscribe((todoData) => {
       this.todos = todoData['todos']
     })
+    
   }
 
   deleteApiTodo(todo: Todo){
@@ -111,4 +38,11 @@ export class TodolistComponent implements OnInit {
 
   }
 
+
+  addApiTodo(new_todo: Todo){
+    this.apiService
+    .addTodo(new_todo)
+    .subscribe(todo => this.todos.push(todo))
+
+  }
 }
