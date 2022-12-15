475188b1d6bb259a30992aee6c5baa80329293e2
Thiago Vasconcelos <pedrosav14@gmail.com>
2022-12-14 22:35:18 -0300

    model updated to sync with the python backend

--- src/models/todo.model.ts
+++ src/models/todo.model.ts
@@ -1,12 +1,22 @@
 export class Todo {
 
+    public parameter = {
+        id: 0,
+        title: "",
+        category: "",
+        done: false
+    };
+
     constructor(
         public id: Number,
         public title: String,
         public category: String,
         public done: Boolean
-
     ) {
 
+        this.parameter.id = Number(id);
+        this.parameter.title = String(title);
+        this.parameter.category = String(category);
+        this.parameter.done = Boolean(done);
     }
 }
