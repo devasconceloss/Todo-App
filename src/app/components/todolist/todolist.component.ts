885dc98bb08424ea16682dd498573f9e9c0156cf
Thiago Vasconcelos <pedrosav14@gmail.com>
2023-01-10 15:13:30 -0300

    refactor: load undone and done todos on init

--- src/app/components/todolist/todolist.component.ts
+++ src/app/components/todolist/todolist.component.ts
@@ -2,7 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Todo } from 'src/models/todo.model';
 import { animate, trigger, style, transition } from '@angular/animations';
 import { ApiService } from '../../services/api.service';
-import { map } from 'rxjs';
+import { catchError, map, Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-todolist',
@@ -11,6 +11,7 @@ import { map } from 'rxjs';
 })
 export class TodolistComponent implements OnInit {
   todos: Todo[] = [];
+  doneTodos: Todo[] = [];
   todo: Todo;
   arraySize: number;
 
@@ -18,21 +19,22 @@ export class TodolistComponent implements OnInit {
   }
 
 
-  ngOnInit() {
-    this.apiService
-    .getTodos().pipe(
-      map(todos => todos.filter(todo => !todo.done))
-    )
-    .subscribe((todoData) => {
-      this.todos = this.todos.concat(todoData);
-      this.setTodos(this.todos)
-      });
-
+  ngOnInit() {  
+      this.loadUndoneTodos()
+      this.loadDoneTodos()
   }
   
 
   setTodos(localtodos: Todo[]){
     this.arraySize = localtodos.length
+  }
+  
+  addApiTodo(new_todo: Todo){
+    this.apiService
+    .addTodo(new_todo)
+    .subscribe(todo => this.todos.push(todo))
+
+    this.arraySize += 1
   }
 
   deleteApiTodo(todo: Todo){
@@ -42,14 +44,6 @@ export class TodolistComponent implements OnInit {
       () => (this.todos) = this.todos.filter((t) => t.id != todo.id))
     
     this.arraySize -= 1
-  }
-
-  addApiTodo(new_todo: Todo){
-    this.apiService
-    .addTodo(new_todo)
-    .subscribe(todo => this.todos.push(todo))
-
-    this.arraySize += 1
   }
 
   finishApiTodo(todo: Todo) {
@@ -62,15 +56,35 @@ export class TodolistComponent implements OnInit {
     this.arraySize -= 1
   }
 
-  showDoneTodos(){
+  loadUndoneTodos(){
     this.apiService
-    .getTodos().pipe(
-      map(todos => todos.filter(todo => !todo.done))
-    )
-    .subscribe((todoData) => {
-      this.todos = this.todos.concat(todoData);
-      this.setTodos(this.todos)
+      .getTodos()
+      .pipe(
+        catchError((err) => {
+          console.log(`API error: ${err}`);
+          return of([] as Todo[]);
+        }),
+        map((todos) => todos.filter((todo) => !todo.done))
+      )
+      .subscribe((todoData) => {
+          this.todos = this.todos.concat(todoData);
       });
-      
+
   }
+
+  loadDoneTodos() {
+    this.apiService
+      .getTodos()
+      .pipe(
+        catchError((err) => {
+          console.log(`API error: ${err}`);
+          return of([] as Todo[]);
+        }),
+        map((todos) => todos.filter((todo) => todo.done))
+      )
+      .subscribe((todoData) => {
+          this.doneTodos = this.doneTodos.concat(todoData);
+          console.log(this.doneTodos);
+      });
+  }  
 }
